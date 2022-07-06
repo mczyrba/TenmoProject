8ee1233d304bf7d9089c7db5568822d3f36d7941
Shannon Dawes <shannondawes@yahoo.com>
2022-07-06 11:38:30 -0400

    created transfer and account models and DAOs, created an account controller

---
+++ capstone/TenmoServer/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TenmoServer.DAO;
+
+namespace TenmoServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IUserDao userDao;
+
+        public AccountController(IUserDao _userDao)
+        {
+            userDao = _userDao;
+        }
+    }
+}
